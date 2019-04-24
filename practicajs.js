3e28a9c907ceea1e65e1d6f724e98fa33440f11e
Danny Cevallos <dannyacv10@javerianacali.edu.co>
2019-04-23 19:03:08 -0500

    Update practicajs.js
    se agregan clases

--- practicajs.js
+++ practicajs.js
@@ -21,6 +21,11 @@ movie.prototype.showInfo = function(){
 	console.log(this.nameMovie,this.yearMovie);
 }
 
+function serie(name,year){
+	movie.call(this,name),
+	movie.call(this,year)
+}
+
 
 
 netflixData.getInfo.setNameMovie('Popeye');
@@ -31,4 +36,33 @@ console.log(netflixData.getInfo.yearMovie);
 
 
 var pelicula = new movie("El regreso del coste","1999");
+var serie1 = new serie("El regreso del coste","1999");
 console.log(pelicula.nameMovie,pelicula.yearMovie);
+serie.prototype=Object.create(movie.prototype);
+
+
+
+class movie{
+
+	constructor(name,year){
+		this.name=name;
+		this.year=year;
+	}
+
+	showInfo(){
+		console.log("mi pelicula: " + this.name + "año: " + this.year);
+	}
+
+
+}
+
+/*var movie1= new constructor("123","123");
+movie1.showInfo();
+*/
+
+class serie extends movie{
+
+} 
+
+var s = new serie("costeño",1992);
+s.showInfo();
